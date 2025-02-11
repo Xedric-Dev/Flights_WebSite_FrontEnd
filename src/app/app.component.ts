d0f0eb30d475f85817e10dc5a2534489cbf4437d
Xedric_Techs <federicobarnaba@gmail.com>
2025-02-11 18:41:25 +0100

    Tried to change some code to make it working on publishing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
